5ad3dea268308f7ab14b05c5cf93f448ba88fb7c
faria98 <faria.annasha@northsouth.edu>
2020-11-06 18:10:42 +0600

    configure database

--- application/views/user/login.php
+++ application/views/user/login.php
@@ -1,3 +1,6 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,7 +11,24 @@
 <body>
 
 <div id="container">
-	<h1>User Login page</h1>
+	<h1>Login page</h1>
+
+	<form role="form" method="post" action="<?php echo base_url('register'); ?>">
+		
+      <div class="form-group">
+      <h3>User Id</h3>
+      <input class="form-control" placeholder="Please enter user id" name="userid" type="text" autofocus>
+      <h3>User Name</h3>
+      <input class="form-control" placeholder="Please enter user name" name="username" type="text" autofocus>
+	  <h3>Email</h3>
+      <input class="form-control" placeholder="Please enter email" name="email" type="text" autofocus>
+	  <h3>Password</h3>
+      <input class="form-control" placeholder="Please enter password" name="password" type="text" autofocus>
+
+	  <h3><input type="submit" value="Login" name="login" ></h3>
+	</form>
+	</div>
+	
 	
 </div>
 
--- application/views/user/registration.php
+++ application/views/user/registration.php
@@ -1,3 +1,6 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,7 +12,37 @@
 
 <div id="container">
 	<h1>Registration page</h1>
+
 	
+	<form role="form" method="post" action="<?php echo base_url('register'); ?>">
+		
+      <div class="form-group">
+      <h3>User Id</h3>
+      <input class="form-control" placeholder="Please enter user id" name="userid" type="text" autofocus>
+      <h3>First Name</h3>
+      <input class="form-control" placeholder="Please enter First Name" name="firstname" type="text" autofocus>
+      <h3>Last Name</h3>
+      <input class="form-control" placeholder="Please enter Last Name" name="lastname" type="text" autofocus>
+      <h3>Gender</h3>
+      <input class="form-control" placeholder="Please enter gender" name="gender" type="text" autofocus>
+      <h3>Designation</h3>
+      <input class="form-control" placeholder="Please enter designation" name="designation" type="text" autofocus>
+      <h3>Birth Date</h3>
+      <input class="form-control" placeholder="Please enter birth date" name="birthdate" type="text" autofocus>
+      <h3>Address</h3>
+      <input class="form-control" placeholder="Please enter address" name="address" type="text" autofocus>
+      <h3>District</h3>
+      <input class="form-control" placeholder="Please enter district" name="district" type="text" autofocus>
+      <h3>Phone No</h3>
+      <input class="form-control" placeholder="Please enter phone no" name="phoneno" type="text" autofocus>
+      <h3>Session</h3>
+      <input class="form-control" placeholder="Please enter session" name="session" type="text" autofocus>
+
+      <h3><input type="submit" value="Register" name="register" ></h3>
+      
+
+      </div>
+	</form>
 </div>
 
 </body>
